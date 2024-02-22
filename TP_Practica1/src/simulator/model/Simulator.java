9d5178a61667bfd0826b1e828ffe818f521c98d8
usuario_local <usuario_local@PTO0113.fdi.ucm.es>
2024-02-22 10:36:49 +0100

    Trabajo hecho en clase. Simulador casi terminado y empezado Controller

--- TP_Practica1/src/simulator/model/Simulator.java
+++ TP_Practica1/src/simulator/model/Simulator.java
@@ -1,6 +1,8 @@
 package simulator.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import org.json.JSONArray;
@@ -29,7 +31,8 @@ public class Simulator implements JSONable {
 	
 	private void set_region(int row, int col, JSONObject r_json) {
 		//llamar a las factorias
-		JSONArray animales = r_json.getJSONArray("animals");
+		//TODO
+		/*JSONArray animales = r_json.getJSONArray("animals");
 		Region r = new Region();
 		for(Animal a : animales.keySet()) {
 			
@@ -41,14 +44,61 @@ public class Simulator implements JSONable {
 		
 		Region r = (Region) r_json.getJSONArray("animals");
 		set_region(row, col, r);
-		Region r2 = new Region();
+		Region r2 = new Region();*/
 		}
 	
 	private void add_animal(Animal a) {
-		
+		_animals_in_list.add(a);
+		_region_mngr.register_animal(a);
 	}
 	
 	public void add_animal(JSONObject a_json) {
+		//llamar a las factorias
+		//TODO
+	}
+	
+	public MapInfo get_map_info() {
+		return this._region_mngr;
+	}
+	
+	public List<? extends AnimalInfo> get_animals(){
+		//List<String> unModifiableStringList = Collections.unmodifiableList(myActualModifiableList);
+		return Collections.unmodifiableList(this._animals_in_list);
+	}
+	
+	public double get_time() {
+		return dt;
+	}
+	
+	public void advance(double dt) {
+		//Puede petar al recorrer la lista
+		this.dt += dt;
+		Iterator<Animal> it = this._animals_in_list.iterator();
+		while (it.hasNext()) {
+			Animal a = it.next();
+			if (a.get_state() == State.DEAD) {
+				_region_mngr.unregister_animal(a); //Eliminamos el animal del region manager
+				it.remove(); //Lo eliminamos de la lista de animales
+			}
+		}
+		
+		for (Animal a : _animals_in_list) {
+			a.update(dt);
+		}		
+		this._region_mngr.update_all_regions(dt);
+		for (Animal a : _animals_in_list) {
+			if (a.is_pregnant()) {
+				add_animal(a.deliver_baby());
+			}	
+		}
+		
+	}
+	
+	public JSONObject as_JSON() {
+		JSONObject jo = new JSONObject();
+		jo.put("time", dt);
+		jo.put("state", this._region_mngr.as_JSON());
+		return jo;
 		
 	}
 }
